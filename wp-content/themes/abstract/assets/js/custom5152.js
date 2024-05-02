47a19f26a21f3327ad41db20e9f70e4dfe567b6f
hitonexsolutions <127612869+hitonexsolutions@users.noreply.github.com>
2024-05-02 15:43:25 +0530

    Add files via upload

--- wp-content/themes/abstract/assets/js/custom5152.js
+++ wp-content/themes/abstract/assets/js/custom5152.js
@@ -367,4 +367,23 @@
         $('.panel-group').on('shown.bs.collapse', toggleIcon);
 
     });
+
+
+
+    
+    var btn = $('#top-button');
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 300) {
+        btn.addClass('show');
+      } else {
+        btn.removeClass('show');
+      }
+    });
+    
+    btn.on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: 0 }, '300');
+    });
 })(jQuery);
+
+
